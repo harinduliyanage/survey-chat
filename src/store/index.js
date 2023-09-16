4314a52820a31e3f0aed9d1568e2a161325f54ab
Nipuna Akalana Perera <nipuna@surge.global>
2023-09-16 15:48:57 +0530

    Removed unwanted codes from the store file

--- src/store/index.js
+++ src/store/index.js
@@ -7,44 +7,15 @@ import {
   FLUSH,
   PAUSE,
   PERSIST,
-  persistReducer,
   PURGE,
   REGISTER,
   REHYDRATE,
   persistStore,
 } from 'redux-persist';
-import { encryptTransform } from 'redux-persist-transform-encrypt';
-import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
-import storage from 'redux-persist/lib/storage';
-import { ENVIRONMENT } from 'config';
-import logger from 'modules/common/utils/logger';
 import { notificationSlice } from 'modules/common/notifications/slice';
 import { dashboardSlice } from 'modules/dashboard/slice';
 
 export const sagaMiddleware = createSagaMiddleware();
-/**
- * Setup encryption using specific key
- */
-const encryption = encryptTransform({
-  secretKey: ENVIRONMENT.PERSIST_ENCRYPTED_SECRET_KEY,
-  onError(error) {
-    logger.error(error);
-  },
-});
-/**
- * White listed slices which needs to be persist with encryption
- */
-const persistConfig = {
-  version: ENVIRONMENT.PERSIST_VERSION,
-  key: ENVIRONMENT.PERSIST_KEY,
-  storage,
-  stateReconciler: hardSet,
-  transforms: [encryption],
-  whitelist: ['feature/base-auth', 'feature/static-data'],
-  writeFailHandler(error) {
-    logger.error(error);
-  },
-};
 /**
  * Combine all the persist and non persist reducers
  */
@@ -53,14 +24,10 @@ const reducers = combineReducers({
   'feature/dashboard': dashboardSlice.reducer,
 });
 /**
- * Define what are the slice which needs to be persist
- */
-const persistedReducer = persistReducer(persistConfig, reducers);
-/**
  * Register all the slices into the main store with encryption
  */
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: reducers,
   middleware: (getDefaultMiddleware) => [
     ...getDefaultMiddleware({
       serializableCheck: {
