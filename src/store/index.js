720ed958c26b4f956ae50654021d7a6a9a198d40
Nipuna Akalana Perera <nipuna@surge.global>
2023-09-16 19:17:14 +0530

    Removed unwanted slices

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from 'root-saga';
-import customMiddleware from 'modules/common/middleware/custom-middleware';
-import { notificationSlice } from 'modules/common/notifications/slice';
 import { businessInfoSlice } from 'modules/business-info/slice';
 import { chatSlice } from 'modules/chat/slice';
 
@@ -12,7 +10,6 @@ export const sagaMiddleware = createSagaMiddleware();
  * Combine all the persist and non persist reducers
  */
 const reducers = combineReducers({
-  'feature/notification': notificationSlice.reducer,
   'feature/business-info': businessInfoSlice.reducer,
   'feature/chat': chatSlice.reducer
 });
@@ -23,7 +20,6 @@ const store = configureStore({
   reducer: reducers,
   middleware: () => [
     sagaMiddleware,
-    customMiddleware,
   ],
 });
 sagaMiddleware.run(rootSaga);
